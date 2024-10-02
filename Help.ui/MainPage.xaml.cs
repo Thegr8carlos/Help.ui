1af327a370d6162d662c98073888947dd54dac35
Carlos <Carlosrams2@outlook.es>
2024-10-02 17:41:29 -0500

    Image of app and some basic changes

--- Help.ui/MainPage.xaml.cs
+++ Help.ui/MainPage.xaml.cs
@@ -2,23 +2,11 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Presionaste {count} veces";
-		else
-			CounterBtn.Text = $"prsionado {count} veces";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
 }
 
