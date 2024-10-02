397be5dbdfff7082d5defff8758d3bf717426e18
Carlos <Carlosrams2@outlook.es>
2024-10-02 14:27:06 -0500

    Borrando archivos innecesarios

--- Help.ui/MainPage.xaml.cs
+++ Help.ui/MainPage.xaml.cs
@@ -14,9 +14,9 @@ public partial class MainPage : ContentPage
 		count++;
 
 		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
+			CounterBtn.Text = $"Presionaste {count} veces";
 		else
-			CounterBtn.Text = $"Clicked {count} times";
+			CounterBtn.Text = $"prsionado {count} veces";
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
