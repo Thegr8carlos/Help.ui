0ac890a801ebc7e71ee7598c0fc8d947a1809798
Carlos <Carlosrams2@outlook.es>
2024-10-07 18:42:57 -0500

    Add some UI functionalities

--- Help.ui/App.xaml.cs
+++ Help.ui/App.xaml.cs
@@ -3,10 +3,12 @@ using Microsoft.Maui.Storage; // used to store if the asssistant is still active
 public partial class App : Application
 {
     public bool IsAssistantActive { get; set; }
+    public bool IsVoiceEnabled { get; set; }
     public App()
 	{
 		InitializeComponent();
         IsAssistantActive = Preferences.Get("IsAssistantActive", false);
+        IsVoiceEnabled = Preferences.Get("IsVoiceEnabled",true);
         MainPage = new AppShell();
 	}
 }
--- Help.ui/MainPage.xaml.cs
+++ Help.ui/MainPage.xaml.cs
@@ -7,9 +7,8 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
         // checks the stored value in the system if not exists then sets to false
-        AssistanOn.IsToggled = (Application.Current as App)?.IsAssistantActive ?? false; 
-
-
+        AssistanOn.IsToggled = (Application.Current as App)?.IsAssistantActive ?? false;
+        SetIsVoiceEnabled((Application.Current as App)?.IsVoiceEnabled ?? true);
     }
 
     void SwitchChanged(object sender, ToggledEventArgs e)
@@ -18,30 +17,71 @@ public partial class MainPage : ContentPage
         {
             // must display the button
             Preferences.Set("IsAssistantActive", e.Value);
+            SetStateMessage(e.Value);
             StartFloatingButtonService();
         }
         else
         {
             //must delete the button
             Preferences.Set("IsAssistantActive", e.Value);
+            SetStateMessage(e.Value);
             StopFloatingButtonService();
+        }
+
+    }
+
+    private void SetStateMessage(bool state)
+    {
+        if (state)
+        {
+            StateMessage.Text = "Activado";
+            StateMessage.TextColor = Colors.Purple;
+        }else
+        {
+            StateMessage.Text = "Desactivado";
+            StateMessage.TextColor = Colors.Black;
         }
     }
 
+    private void SetIsVoiceEnabled(bool enabled)
+    {
+        if (enabled)
+        {
+            AppMode.Source = "voice.svg";
+        }else
+        {
+            AppMode.Source = "text.svg";
+        }
+    }
     private void StartFloatingButtonService()
     {
-        // Crear un intent para iniciar el servicio
+        // starts service
         var intent = new Intent(Android.App.Application.Context, typeof(FloatingButtonService));
         Android.App.Application.Context.StartService(intent);
-        DisplayAlert("Estado del asistente", "Servicio activado", "OK");
+        //DisplayAlert("Estado del asistente", "Servicio activado", "OK");
     }
 
     private void StopFloatingButtonService()
     {
-        // Crear un intent para detener el servicio
+        //stops the service
         var intent = new Intent(Android.App.Application.Context, typeof(FloatingButtonService));
         Android.App.Application.Context.StopService(intent);
-        DisplayAlert("Estado del asistente", "Servicio desactivado.", "OK");
+        //DisplayAlert("Estado del asistente", "Servicio desactivado.", "OK");
+    }
+
+    private void OnModeTapped(object sender, EventArgs e)
+    {
+
+        (Application.Current as App).IsVoiceEnabled = !(Application.Current as App).IsVoiceEnabled;
+
+        Preferences.Set("IsVoiceEnabled", (Application.Current as App).IsVoiceEnabled);
+
+        SetIsVoiceEnabled((Application.Current as App).IsVoiceEnabled);
+    }
+
+    private void OnSettingsTapped(object sender, EventArgs e)
+    {
+        //
     }
 
 }
